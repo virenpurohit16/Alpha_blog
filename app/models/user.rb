3e5258da7c9375877d0e534fd7842d065bf7950a
virenpurohit16 <virenpurohit16@gmail.com>
2022-02-15 07:59:50 +0530

    Adding secure password(has_secure_password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ before_save { self.email = email.downcase }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
                                             length: { maximum: 25 },
                                             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_164750) do
+ActiveRecord::Schema.define(version: 2022_02_15_021621) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_164750) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
